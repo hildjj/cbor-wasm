51515958a615f99645f2fc50c0d56938233ab0c9
Joe Hildebrand <joe-github@cursive.net>
2021-03-24 01:37:06 -0600

    most test vectors working.  streaming objects broken.

--- test/test.mjs
+++ test/test.mjs
@@ -1,6 +1,7 @@
 import {Decoder, Tag} from '../lib/cbor.mjs'
 import assert from 'assert'
 import util from 'util'
+import fs from 'fs'
 
 const cases = [
   ['00', 0],
@@ -20,7 +21,7 @@ const cases = [
   ['a16461626364820001', {abcd: [0, 1]}],
   ['491c0000000000000000', Decoder.hex('1c0000000000000000')],
   ['81491c0000000000000000', [Decoder.hex('1c0000000000000000')]],
-  ['c3491c0000000000000000', new Tag(3, Decoder.hex('1c0000000000000000'))],
+  ['c9491c0000000000000000', new Tag(9, Decoder.hex('1c0000000000000000'))],
   ['fb3ff3333333333333', 1.2],
   ['fbbff3333333333333', -1.2],
   ['fb7ff8000000000000', NaN],
@@ -46,6 +47,11 @@ async function main() {
   })
   await d.init()
 
+  // this is a hack to look at the current parser state.  it must
+  // be checked after write() returns, when you know a single complete
+  // item has been written.
+  const parser_state = new Int32Array(d.mod.memory.buffer, d.parser, 8)
+
   function parse(str) {
     res = NONE
     d.write(str)
@@ -53,20 +59,18 @@ async function main() {
       throw new Error(`DID NOT FINISH: "${str}"`)
     }
     // make sure we don't have to call reset() after every item.
-    // this is a hack to look at the current parser state.  it must
-    // be checked after write() returns, when you know a single complete
-    // item has been written.
-    const pbuf = new Int32Array(d.mod.memory.buffer, d.parser, 8)
-    assert.equal(pbuf[0], 8) // START
-    assert.equal(pbuf[1], 0)
+    assert.equal(parser_state[0], 8) // START
+    assert.equal(parser_state[1], 0)
 
     return res
   }
 
   let ok = 0
   let notOk = 0
+  let total = 0
   for (const [actual, expected] of cases) {
     try {
+      total++
       assert.deepEqual(parse(actual), expected, actual)
       console.log(`ok ${ok++} ${actual}`)
     } catch (e) {
@@ -78,9 +82,89 @@ async function main() {
       d.reset()
     }
   }
+
+  const appendix_a = new URL('../test-vectors/appendix_a.json', import.meta.url)
+  let vectors = []
+  try {
+    let txt = await fs.promises.readFile(appendix_a, 'utf8')
+    // HACK: don't lose data when JSON parsing
+    txt = txt.replace(/"decoded":\s*(-?\d+(\.\d+)?(e[+-]\d+)?)\n/g,
+      `"decoded": {
+        "___TYPE___": "number",
+        "___VALUE___": "$1"
+      }
+  `)
+    vectors = JSON.parse(txt, (key, value) => {
+      if (!value) {
+        return value
+      }
+      switch (value['___TYPE___']) {
+        case 'number': {
+          const v = value['___VALUE___']
+          if (/^-?\d+$/.test(v)) {
+            const b = BigInt(v)
+            if (b > 0) {
+              return (b <= Number.MAX_SAFE_INTEGER) ? Number(b) : b
+            }
+            return (b >= Number.MIN_SAFE_INTEGER) ? Number(b) : b
+          }
+          return Number.parseFloat(v)
+        }
+        default:
+          return value
+      }
+    })
+  } catch (e) {
+    console.log(`not ok ${notOk++}
+  ---
+    Run "git submodule update --init"
+    ${util.inspect(e).replace(/\n/g, '\n    ')}
+  ...
+      `)
+  }
+  for (const {hex, decoded, diagnostic} of vectors) {
+    try {
+      total++
+      let skip = true
+      if (decoded != null) {
+        skip = false
+        assert.deepEqual(
+          parse(hex),
+          decoded,
+          hex
+        )
+      }
+      if ((diagnostic != null) && (![
+        // real diagnose
+        'c074323031332d30332d32315432303a30343a30305a',
+        'd74401020304',
+        'd818456449455446',
+        'd82076687474703a2f2f7777772e6578616d706c652e636f6d',
+        '40',
+        '4401020304',
+        'a201020304',
+        '5f42010243030405ff'
+      ].includes(hex))) {
+        skip = false
+        assert.deepEqual(
+          util.inspect(parse(hex), {colors: false, depth: Infinity}),
+          diagnostic,
+          hex
+        )
+      }
+      console.log(`${skip ? 'skip' : 'ok'} ${ok++} ${hex}`)
+    } catch (e) {
+      console.log(`not ok ${notOk++} ${hex}
+  ---
+    ${util.inspect(e).replace(/\n/g, '\n    ')}
+  ...
+      `)
+      d.reset()
+    }
+  }
   console.log(`
-${notOk}..${cases.length}
-# tests ${cases.length}
+${notOk}..${total}
+# tests ${total}
 # pass: ${ok}
 # fail: ${notOk}
 `)
